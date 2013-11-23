24f03a3c0c6ac77a5f0905b2a8a273505cfb89ab
yolinlin <413681487@qq.com>
2013-11-23 14:47:41 +0800

    * Core/Misc: Fix a compile warning.
    Closes #11229

--- src/server/game/Entities/Player/Player.cpp
+++ src/server/game/Entities/Player/Player.cpp
@@ -18273,7 +18273,7 @@ bool Player::LoadFromDB(uint32 guid, SQLQueryHolder *holder)
         }
         else
         {
-            TC_LOG_ERROR("entities.player", "Player (guidlow %d) have problems with transport guid (%u). Teleport to bind location.",
+            TC_LOG_ERROR("entities.player", "Player (guidlow %u) have problems with transport guid (%u). Teleport to bind location.",
                 guid, transGUID);
 
             RelocateToHomebind();
